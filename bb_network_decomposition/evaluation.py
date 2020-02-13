fc53d1665f375995a8d1b881596b8d18504caa20
Benjamin Wild <b.w@fu-berlin.de>
2020-02-13 08:58:08 +0100

    Fix future predictability and add bee subsampling analysis

--- bb_network_decomposition/evaluation.py
+++ bb_network_decomposition/evaluation.py
@@ -3,6 +3,12 @@ import numpy as np
 import pandas as pd
 import sklearn
 import sklearn.model_selection
+import sklearn.linear_model
+
+from bb_network_decomposition import normalization
+from bb_network_decomposition import spectral
+from bb_network_decomposition import data
+from bb_network_decomposition import projection
 
 
 location_labels = [
@@ -19,7 +25,8 @@ default_factors = [
 ]
 
 
-def evaluate_network_factors(df, model, n_splits=25, groupby=None, labels=location_labels, factors=default_factors):
+def evaluate_network_factors(df, model=None, n_splits=25, groupby=None, labels=location_labels,
+                             factors=default_factors, scoring=None):
     def get_factor(factor):
         if "+" in factor:
             return np.stack([get_factor(f) for f in factor.split("+")], axis=1)
@@ -30,6 +37,12 @@ def evaluate_network_factors(df, model, n_splits=25, groupby=None, labels=locati
         if is_log:
             X = np.log1p(X)
         return X
+
+    if model is None:
+        model = sklearn.linear_model.LinearRegression()
+
+    if scoring is None:
+        scoring = sklearn.metrics.make_scorer(sklearn.metrics.r2_score)
 
     if groupby is None:
         groups = None
@@ -57,7 +70,7 @@ def evaluate_network_factors(df, model, n_splits=25, groupby=None, labels=locati
             scores = sklearn.model_selection.cross_val_score(
                 model, X, Y,
                 cv=cv, groups=groups,
-                scoring=sklearn.metrics.make_scorer(sklearn.metrics.r2_score),
+                scoring=scoring,
                 n_jobs=-1
             )
 
@@ -76,9 +89,10 @@ def evaluate_network_factors(df, model, n_splits=25, groupby=None, labels=locati
     return regression_results_df, regression_pivot
 
 
-def evaluate_future_predictability(df, model, days_into_future=0, labels=location_labels, factors=default_factors, evaluation_kws={}):
-    df_copy = df.copy()
-    df_copy.date += datetime.timedelta(days=days_into_future)
+def evaluate_future_predictability(df, factors=default_factors, labels=location_labels, days_into_future=0,
+                                   min_age=0, max_age=100, evaluation_kws={}):
+    df_shifted = df.copy()
+    df_shifted.date -= datetime.timedelta(days=days_into_future)
 
     raw_factors = []
     for f in factors:
@@ -90,7 +104,35 @@ def evaluate_future_predictability(df, model, days_into_future=0, labels=locatio
         else:
             raw_factors.append(f)
 
-    df_copy = df_copy[['date', 'bee_id'] + labels].merge(df[['date', 'bee_id'] + raw_factors], on=('date', 'bee_id'))
+    df_idxer = (df.age >= min_age) & (df.age < max_age)
+    df_shifted = df_shifted[['date', 'bee_id'] + labels].merge(df[df_idxer][['date', 'bee_id', 'day'] + raw_factors], on=('date', 'bee_id'))
 
-    return evaluate_network_factors(df_copy, model, labels=labels, factors=factors, **evaluation_kws)
+    return evaluate_network_factors(df_shifted, **evaluation_kws), df_shifted
+
+
+def evaluate_bee_subsample(keep_proportion, interactions, alive_matrices, bee_ids, bee_ages, alive_df,
+                           from_date, num_factors_per_mode, location_dataframe, evaluation_kws={}):
+    num_days = interactions.shape[0]
+    num_entities = int(interactions.shape[1] * keep_proportion)
+    keep_entities = sorted(np.random.choice(
+        range(interactions.shape[1]),
+        size=num_entities,
+        replace=False))
+
+    alive_matrices = alive_matrices[np.ix_(range(num_days), keep_entities, keep_entities)]
+    interactions = interactions[np.ix_(range(num_days), keep_entities, keep_entities, range(interactions.shape[-1]))].copy()
+    bee_ids = bee_ids[keep_entities].copy()
+    bee_ages = bee_ages[:, keep_entities].copy()
+    alive_df = alive_df[alive_df.bee_id.isin(bee_ids)].sort_values('bee_id')
+
+    interactions = normalization.rank_transform(interactions, alive_matrices)
+    daily_factors, _ = spectral.decomposition_by_day(
+        interactions, alive_matrices, num_factors_per_mode, num_jobs=4
+    )
+    daily_factors_aligned = spectral.temporal_alignment(daily_factors, alive_matrices)
+    factor_df = data.get_factor_dataframe(daily_factors_aligned, from_date, alive_df, bee_ids)
+
+    cca_factor_df = projection.get_cca_projection(factor_df, location_dataframe)
+
+    return evaluate_network_factors(cca_factor_df, **evaluation_kws)
 
