1b5af2debe65493c2ff06ad91799b1cfd7b25838
Benjamin Wild <b.w@fu-berlin.de>
2020-02-17 09:28:30 +0100

    Make CCA targets configurable

--- bb_network_decomposition/projection.py
+++ bb_network_decomposition/projection.py
@@ -6,6 +6,7 @@ import sklearn.decomposition
 import sklearn.preprocessing
 
 import bb_network_decomposition.data
+import bb_network_decomposition.evaluation
 
 
 def scale_projection_by_day(factor_df, column, inplace=False, how='percentiles'):
@@ -49,23 +50,20 @@ def get_pca_projection(factor_df, num_components=2, inplace=False, scale_by_day=
     return factor_df
 
 
-def get_cca_projection(factor_df, location_df, num_components=2, inplace=False, scale_by_day='percentiles', cca=None, return_cca=False):
+def get_cca_projection(factor_df, location_df, num_components=2, inplace=False,
+                       scale_by_day='percentiles', cca=None, return_cca=False,
+                       target_cols=bb_network_decomposition.evaluation.location_labels):
     if not inplace:
         factor_df = factor_df.copy()
 
     merged_df = bb_network_decomposition.data.merge_location_data(factor_df, location_df)
 
     factors = bb_network_decomposition.data.factors_from_dataframe(merged_df)
-    targets = np.stack((
-        merged_df.brood_area,
-        merged_df.dance_floor,
-        merged_df.honey_storage,
-        merged_df.near_exit,
-    ), axis=0)
+    targets = merged_df[target_cols].values
 
     if cca is None:
-        cca = sklearn.cross_decomposition.CCA(n_components=num_components).fit(factors, targets.T)
-    factor_projection, location_projection = cca.transform(factors, targets.T)
+        cca = sklearn.cross_decomposition.CCA(n_components=num_components).fit(factors, targets)
+    factor_projection, location_projection = cca.transform(factors, targets)
 
     merged_df['network_age'] = factor_projection[:, 0]
     scale_projection_by_day(merged_df, 'network_age', inplace=True, how=scale_by_day)
