9fb053a3beb830e476ebc470257e17f456ca40d6
Benjamin Wild <b.w@fu-berlin.de>
2020-02-25 10:26:54 +0100

    Add knn_transform
    Optionally use kNN representation for subset of interaction modes

--- bb_network_decomposition/normalization.py
+++ bb_network_decomposition/normalization.py
@@ -1,13 +1,21 @@
 import numpy as np
 import scipy
 import scipy.stats
+import sklearn
+import sklearn.neighbors
+
+
+def get_alive_idxer(alive_matrices, day, mode):
+    day_alive = alive_matrices[day].max(axis=-1)
+    ix = np.ix_([day], np.argwhere(day_alive)[:, 0], np.argwhere(day_alive)[:, 0], [mode])
+
+    return ix
 
 
 def alive_graph_transform(interactions, alive_matrices, transform):
     for mode in range(interactions.shape[-1]):
         for day in range(interactions.shape[0]):
-            day_alive = alive_matrices[day].max(axis=-1)
-            ix = np.ix_([day], np.argwhere(day_alive)[:, 0], np.argwhere(day_alive)[:, 0], [mode])
+            ix = get_alive_idxer(alive_matrices, day, mode)
             alive_graph = interactions[ix]
 
             interactions[ix] = transform(alive_graph)
@@ -33,3 +41,31 @@ def std_transform(interactions, alive_matrices):
         return flat_graph.reshape(alive_graph.shape)
 
     return alive_graph_transform(interactions, alive_matrices, _transform)
+
+
+def knn_transform(interactions, alive_matrices, mode_subset=None):
+    if mode_subset is None:
+        mode_subset = range(interactions.shape[-1])
+
+    for mode in mode_subset:
+        for day in range(interactions.shape[0]):
+            day_alive = alive_matrices[day].max(axis=-1)
+            ix = np.ix_([day], np.argwhere(day_alive)[:, 0], np.argwhere(day_alive)[:, 0], [mode])
+
+            is_greater_zero = (interactions[day, :, :, mode] > 0).astype(np.float)
+
+            connectivity = sklearn.neighbors.KNeighborsTransformer(
+                mode='connectivity',
+                n_neighbors=int(np.ceil(np.log(interactions.shape[1]))),
+                metric='precomputed'
+            ).fit_transform(-1 * (interactions[day, :, :, mode]) + (interactions[day, :, :, mode].max()))
+            affinity_matrix = (0.5 * (connectivity + connectivity.T)).toarray()
+            affinity_matrix *= is_greater_zero
+
+            alive_affinity_matrix = np.zeros_like(affinity_matrix)
+            alive_affinity_matrix[ix[1:-1]] = affinity_matrix[ix[1:-1]]
+
+            interactions[day, :, :, mode] = alive_affinity_matrix
+
+    return interactions
+
