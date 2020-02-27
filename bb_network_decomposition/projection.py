BioroboticsLab/bb_network_decomposition

commit b67cab803d846d48ac0c6feb83eb22404271acf1

Code readability improvements
Split up long lines.

bb_network_decomposition/projection.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pandas as pd
 import scipy
 import scipy.stats
 import sklearn
@@ -22,13 +21,14 @@ def scale_projection_by_day(factor_df, column, inplace=False, how='percentiles')
 
     for day in factor_df.day.unique():
         idxer = np.argwhere((factor_df.day == day).values)[:, 0], np.argwhere(factor_df.columns == column)[0, 0]
+        factors = factor_df.iloc[idxer]
 
         if how == 'percentiles':
-            factor_df.iloc[idxer] -= np.percentile(factor_df.iloc[idxer], 5)
-            factor_df.iloc[idxer] /= np.percentile(factor_df.iloc[idxer], 95)
+            factor_df.iloc[idxer] -= np.percentile(factors, 5)
+            factor_df.iloc[idxer] /= np.percentile(factors, 95)
             factor_df.iloc[idxer] *= 40
         elif how == 'minmax':
-            factor_df.iloc[idxer] = sklearn.preprocessing.MinMaxScaler().fit_transform(factor_df.iloc[idxer][:, None])[:, 0]
+            factor_df.iloc[idxer] = sklearn.preprocessing.MinMaxScaler().fit_transform(factors[:, None])[:, 0]
             factor_df.iloc[idxer] *= 40
         else:
             assert False

bb_network_decomposition/data.py
@@ -6,6 +6,20 @@ import bb_utils
 import bb_utils.meta
 import bb_utils.ids
 
+
+location_cols = [
+    'brood_area', 'brood_area_open', 'dance_floor', 'honey_storage', 'near_exit'
+]
+
+default_location_data_cols = [
+    'bee_id', 'age', 'brood_area', 'brood_area_open', 'brood_area_combined', 'dance_floor', 'honey_storage', 'near_exit'
+]
+
+default_supplementary_data_cols = [
+    'r_squared', 'day_activity', 'phase', 'amplitude', 'days_left'
+]
+
+
 def load_networks_h5(path, first_day, last_day):
     with h5py.File(path, 'r') as f:
         labels = list(f['labels'])
@@ -21,15 +35,6 @@ def load_alive_data(path, bee_ids):
     alive_df = pd.concat([alive_df[alive_df.bee_id == bee_id] for bee_id in bee_ids])
 
     return alive_df
-
-
-location_cols = [
-    'brood_area', 'brood_area_open', 'dance_floor', 'honey_storage', 'near_exit'
-]
-
-default_location_data_cols = [
-    'bee_id', 'age', 'brood_area', 'brood_area_open', 'brood_area_combined', 'dance_floor', 'honey_storage', 'near_exit'
-]
 
 
 def load_location_data(path, keepcols=default_location_data_cols):
@@ -48,6 +53,10 @@ def load_location_data(path, keepcols=default_location_data_cols):
         loc_df = loc_df[(loc_df[location_cols].sum(axis=1) - 1).abs() < eps]
 
     return loc_df
+
+
+def load_supplementary_data(path, keepcols=default_location_data_cols + default_supplementary_data_cols):
+    return load_location_data(path, keepcols=keepcols)
 
 
 def get_daily_alive_matrices(alive_df, num_days, num_entities, from_date):
@@ -73,15 +82,24 @@ def get_factor_dataframe(daily_factors, from_date, alive_df, bee_ids):
     for day in range(num_days):
         date = from_date + datetime.timedelta(days=day)
 
-        alive_ids = sorted(alive_df[(alive_df.annotated_tagged_date <= date) & (alive_df.inferred_death_date > date)].bee_id.values)
+        alive_df_filter = (alive_df.annotated_tagged_date <= date) & (alive_df.inferred_death_date > date)
+        alive_ids = sorted(alive_df[alive_df_filter].bee_id.values)
 
         bbids = [bb_utils.ids.BeesbookID.from_ferwar(bid) for bid in alive_ids]
         ages = [m.get_age(bid, date).days for bid in bbids]
 
         columns = ['day', 'date', 'bee_id', 'age'] + ['f_{}'.format(f) for f in range(num_factors)]
-        factor_df = pd.DataFrame(np.concatenate((
-            np.array([day for _ in range(len(ages))])[:, None], np.array([date for _ in range(len(ages))])[:, None], np.array(alive_ids)[:, None], np.array(ages)[:, None], daily_factors[day][np.array([(bee_id in alive_ids) for bee_id in bee_ids])]), axis=-1),
-                                columns=columns)
+        factor_df = pd.DataFrame(
+            np.concatenate(
+                (
+                    np.array([day for _ in range(len(ages))])[:, None],
+                    np.array([date for _ in range(len(ages))])[:, None],
+                    np.array(alive_ids)[:, None],
+                    np.array(ages)[:, None],
+                    daily_factors[day][np.array([(bee_id in alive_ids) for bee_id in bee_ids])]
+                ), axis=-1
+            ), columns=columns
+        )
         dfs.append(factor_df)
 
     factor_df = pd.concat(dfs)

bb_network_decomposition/evaluation.py
@@ -105,7 +105,8 @@ def get_timeshifted_df(df, factors=default_factors, labels=location_labels, days
             raw_factors.append(f)
 
     df_idxer = (df.age >= min_age) & (df.age < max_age)
-    df_shifted = df_shifted[['date', 'bee_id'] + labels].merge(df[df_idxer][['date', 'bee_id', 'day'] + raw_factors], on=('date', 'bee_id'))
+    df_shifted = df_shifted[['date', 'bee_id'] + labels].merge(
+        df[df_idxer][['date', 'bee_id', 'day'] + raw_factors], on=('date', 'bee_id'))
 
     return df_shifted
 
@@ -127,7 +128,8 @@ def evaluate_bee_subsample(keep_proportion, interactions, alive_matrices, bee_id
         replace=False))
 
     alive_matrices = alive_matrices[np.ix_(range(num_days), keep_entities, keep_entities)]
-    interactions = interactions[np.ix_(range(num_days), keep_entities, keep_entities, range(interactions.shape[-1]))].copy()
+    interaction_idxer = np.ix_(range(num_days), keep_entities, keep_entities, range(interactions.shape[-1]))
+    interactions = interactions[interaction_idxer].copy()
     bee_ids = bee_ids[keep_entities].copy()
     bee_ages = bee_ages[:, keep_entities].copy()
     alive_df = alive_df[alive_df.bee_id.isin(bee_ids)].sort_values('bee_id')
@@ -142,4 +144,3 @@ def evaluate_bee_subsample(keep_proportion, interactions, alive_matrices, bee_id
     cca_factor_df = bb_network_decomposition.projection.get_cca_projection(factor_df, location_dataframe)
 
     return evaluate_network_factors(cca_factor_df, **evaluation_kws)
-

bb_network_decomposition/normalization.py
@@ -68,4 +68,3 @@ def knn_transform(interactions, alive_matrices, mode_subset=None):
             interactions[day, :, :, mode] = alive_affinity_matrix
 
     return interactions
-

bb_network_decomposition/spectral.py
@@ -43,11 +43,13 @@ def decompose_day(interactions, alive_matrices, num_factors, spectral_kws):
     return all_embeddings_day, num_factors_by_mode
 
 
-def decomposition_by_day(interactions, alive_matrices, num_factors, num_jobs=-1, spectral_kws={'regularization': 0.01, 'scaling': 'divide'}):
+def decomposition_by_day(interactions, alive_matrices, num_factors, num_jobs=-1,
+                         spectral_kws={'regularization': 0.01, 'scaling': 'divide'}):
     num_days = interactions.shape[0]
     assert interactions.shape[1] == interactions.shape[2]
 
-    decompose_wrapper = lambda day: decompose_day(interactions[day], alive_matrices[day], num_factors, spectral_kws)
+    def decompose_wrapper(day):
+        return decompose_day(interactions[day], alive_matrices[day], num_factors, spectral_kws)
 
     if num_jobs == -1:
         num_jobs = multiprocessing.cpu_count()
@@ -91,7 +93,8 @@ def temporal_alignment(daily_factors, alive_matrices, scaler=None):
             day_alive = alive_matrices[day].max(axis=-1)
             both_days_alive = (alive_matrices[day].max(axis=-1) & alive_matrices[day-1].max(axis=-1))
             features_unscaled_day = daily_factors[day].copy()
-            corrs = [scipy.stats.spearmanr(unscaled_features[-1][both_days_alive][:, mode], daily_factors[day][both_days_alive][:, mode]) for mode in range(num_factors)]
+            corrs = [scipy.stats.spearmanr(unscaled_features[-1][both_days_alive][:, mode],
+                                           daily_factors[day][both_days_alive][:, mode]) for mode in range(num_factors)]
 
             corr_signs = np.array([np.sign(c.correlation) for c in corrs])[None, :]
             corr_signs[np.isnan(corr_signs)] = 1.

commit 2fadc4d7be5e51b8cad0094ca45745940518500b

Fix regression introduced in b67cab8

bb_network_decomposition/projection.py
@@ -21,14 +21,14 @@ def scale_projection_by_day(factor_df, column, inplace=False, how='percentiles')
 
     for day in factor_df.day.unique():
         idxer = np.argwhere((factor_df.day == day).values)[:, 0], np.argwhere(factor_df.columns == column)[0, 0]
-        factors = factor_df.iloc[idxer]
 
         if how == 'percentiles':
-            factor_df.iloc[idxer] -= np.percentile(factors, 5)
-            factor_df.iloc[idxer] /= np.percentile(factors, 95)
+            factor_df.iloc[idxer] -= np.percentile(factor_df.iloc[idxer], 5)
+            factor_df.iloc[idxer] /= np.percentile(factor_df.iloc[idxer], 95)
             factor_df.iloc[idxer] *= 40
         elif how == 'minmax':
-            factor_df.iloc[idxer] = sklearn.preprocessing.MinMaxScaler().fit_transform(factors[:, None])[:, 0]
+            factor_df.iloc[idxer] = sklearn.preprocessing.MinMaxScaler().fit_transform(
+                factor_df.iloc[idxer][:, None])[:, 0]
             factor_df.iloc[idxer] *= 40
         else:
             assert False