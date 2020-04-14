BioroboticsLab/bb_network_decomposition

commit 216094fa22795d8aaeeb4efcc1f242b7da44db4e

Use context wrapper for thread pool

bb_network_decomposition/spectral.py
@@ -69,8 +69,8 @@ def decomposition_by_day(
     if num_jobs == -1:
         num_jobs = multiprocessing.cpu_count()
 
-    pool = multiprocessing.pool.ThreadPool(num_jobs)
-    results = pool.imap(decompose_wrapper, range(num_days))
+    with multiprocessing.pool.ThreadPool(num_jobs) as pool:
+        results = pool.imap(decompose_wrapper, range(num_days))
 
     daily_factors = []
     for all_embeddings_day, num_factors_by_mode in results:

commit 82e85aebf44e1c3115cb7c4d7de2f4bdeccfb03d

Fix regression introduced in 216094f
Don't close thread pool before iterating over results

bb_network_decomposition/spectral.py
@@ -72,9 +72,9 @@ def decomposition_by_day(
     with multiprocessing.pool.ThreadPool(num_jobs) as pool:
         results = pool.imap(decompose_wrapper, range(num_days))
 
-    daily_factors = []
-    for all_embeddings_day, num_factors_by_mode in results:
-        daily_factors.append(all_embeddings_day)
+        daily_factors = []
+        for all_embeddings_day, num_factors_by_mode in results:
+            daily_factors.append(all_embeddings_day)
 
     return daily_factors, num_factors_by_mode
 